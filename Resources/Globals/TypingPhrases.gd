6cbd651fd0e2949ea56c06528da70da1b19e309e
Tyler Funk <tcfunk25@gmail.com>
2024-09-19 13:25:56 -0500

    dont add empty phrases to pool

--- Resources/Globals/TypingPhrases.gd
+++ Resources/Globals/TypingPhrases.gd
@@ -5,9 +5,9 @@ extends Node
 func _init():
 	var dictionary_file = FileAccess.open("res://Resources/dictionary.txt", FileAccess.READ)
 	while not dictionary_file.eof_reached():
-		phrases.append(dictionary_file.get_line())
-	
-	print("phrases contains {0} elements".format(phrases.size()))
+		var phrase = dictionary_file.get_line()
+		if not phrase.is_empty():
+			phrases.append(phrase)
 
 func get_random_phrase():
 	return phrases.pick_random()
