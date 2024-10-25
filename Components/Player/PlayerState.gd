eb3b5a1ca0d3a6d4caaeeebddefd3e8b42ccada3
Tyler Funk <tcfunk25@gmail.com>
2024-10-25 13:41:41 -0500

    create basic score multiplier logic

--- Components/Player/PlayerState.gd
+++ Components/Player/PlayerState.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var letter_value_base = 10
-var score : int = 0
-var streak : int = 0
-var multiplier : int = 1
+var letter_value_base := 10
+var score := 0
+var streak := 0
+var multiplier := 1
 
 func _ready():
 	Messenger.correct_letter_typed.connect(_update_player_score)
@@ -15,9 +15,15 @@ func _break_streak():
 
 func _update_player_score():
 	streak += 1
-	_calculate_multiplier()
+	multiplier = _calculate_multiplier()
 	score += letter_value_base * multiplier
 	Messenger.score_changed.emit(streak, score)
 	
-func _calculate_multiplier():
-	multiplier = 1
+func _calculate_multiplier() -> int:
+	if streak >= 200:
+		return 4
+	if streak >= 100:
+		return 3
+	if streak >= 20:
+		return 2
+	return 1
