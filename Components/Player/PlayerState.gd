5681a0167543b2d87d8a816789ea4c9c8b18862c
Tyler Funk <tcfunk25@gmail.com>
2024-12-07 10:22:03 -0600

    track words per minute and errors throughtout level, display in win screen

--- Components/Player/PlayerState.gd
+++ Components/Player/PlayerState.gd
@@ -5,8 +5,16 @@ var score := 0
 var streak := 0
 var bonus := 1
 var health := 5
+var wpm : Array[float]
+
+# These should probably go somewhere else, idk
+var timer_running := false
+var start_time := 0.0
+var letters_typed := 0
+var mistakes := 0
 
 func _ready():
+	Messenger.correct_letter_typed.connect(_start_typing_timer)
 	Messenger.wrong_letter_typed.connect(_break_streak)
 	Messenger.enemy_defeated.connect(_update_player_score)
 	Messenger.player_take_damage.connect(_damage_player)
@@ -14,12 +22,14 @@ func _ready():
 func _break_streak():
 	streak = 0
 	bonus = 0
+	mistakes += 1
 
 func _update_player_score():
 	streak += 1
 	bonus = _calculate_bonus()
 	var gained = letter_value_base + bonus
 	score += gained
+	_track_typing_stats()
 	Messenger.score_changed.emit(streak, score, gained)
 
 func _calculate_bonus() -> int:
@@ -38,3 +48,16 @@ func _damage_player() -> int:
 		Messenger.player_died.emit()
 	return health
 
+func _start_typing_timer():
+	letters_typed += 1
+	if timer_running:
+		return
+	timer_running = true
+	start_time = Time.get_ticks_msec()
+	
+func _track_typing_stats():
+	timer_running = false
+	var type_time = Time.get_ticks_msec() - start_time
+	wpm.append(type_time)
+	print(type_time)
+	
--- Components/UI/WinScreen.gd
+++ Components/UI/WinScreen.gd
@@ -1,7 +1,22 @@
 extends Control
+@onready var wpm_value = $VBoxContainer/HBoxContainer/wpm_value
+@onready var errors_value = $VBoxContainer/HBoxContainer2/errors_value
 
 func _ready():
 	visible = false
 
 func on_level_completed():
+	if visible:
+		return
 	visible = true
+	var num_words = PlayerState.wpm.size()
+	var time_sum = PlayerState.wpm.reduce(func(sum, number): return sum + number)
+	var wpm_score = wpm(PlayerState.letters_typed, time_sum)
+	wpm_value.text = "%d" % wpm_score
+	errors_value.text = "%d" % PlayerState.mistakes
+
+func wpm(letters, time) -> float:
+	return (letters / 5.0) / millis_to_minutes(time)
+
+func millis_to_minutes(millis: float) -> float:
+	return millis / (1000.0 * 60.0)
