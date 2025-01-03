431f749e06d60a6a7f9956df9acb444ac884c2f6
Tyler Funk <tcfunk25@gmail.com>
2025-01-03 16:08:26 -0600

    added steam plugin, working on steam leaderboard integration

---
+++ Scripts/SteamInit.gd
@@ -0,0 +1,32 @@
+extends Node
+
+signal steam_ready
+
+const STEAM_APP_ID = 480
+var leaderboard_handle: int
+
+func _init():
+	OS.set_environment("SteamAppId", str(STEAM_APP_ID))
+	OS.set_environment("SteamGameId", str(STEAM_APP_ID))
+	initialize_steam()
+
+func _process(delta):
+	Steam.run_callbacks()
+	
+func initialize_steam() -> void:
+	var init_response : Dictionary = Steam.steamInitEx()
+	if init_response.status == 0:
+		Steam.leaderboard_find_result.connect(_on_leaderboard_find_result)
+		Steam.findOrCreateLeaderboard("typestrike_leader", Steam.LEADERBOARD_SORT_METHOD_DESCENDING, Steam.LEADERBOARD_DISPLAY_TYPE_NUMERIC)
+		var friend_count = Steam.getFriendCount(Steam.FRIEND_FLAG_ALL)
+		print("you have %d steam friends" % friend_count)
+		for i in friend_count:
+			var friend = Steam.getFriendByIndex(i, Steam.FRIEND_FLAG_ALL)
+			var nickname = Steam.getFriendPersonaName(friend)
+			print("one is named %s (%s)" % [nickname, str(friend)])
+	else:
+		printerr("Steam failed to initialize")
+
+func _on_leaderboard_find_result(handle: int, found: int) -> void:
+	leaderboard_handle = handle
+	steam_ready.emit()
---
+++ Scripts/steam_test.gd
@@ -0,0 +1,62 @@
+extends Control
+
+@onready var submit_score_button = $"Submit Score Button"
+@onready var leaderboard_entries = $"VBoxContainer/leaderboard entries"
+@onready var tree = $Tree
+
+func _ready():
+	submit_score_button.pressed.connect(submit_leaderboard_score)
+	Steam.leaderboard_scores_downloaded.connect(update_leaderboard_scores)
+	Steam.leaderboard_score_uploaded.connect(on_leaderboard_score_uploaded)
+	SteamInit.steam_ready.connect(refresh_entries)
+	var root = tree.create_item()
+	var l1 = tree.create_item(root)
+	var l2 = tree.create_item(root)
+	tree.set_column_title(0, "Current")
+	tree.set_column_title(1, "Your Best")
+	tree.set_column_title(2, "Leaderboard")
+	l1.set_text(0, '12345')
+	l1.set_text(1, '12345')
+	l1.set_text(2, '12345')
+	l2.set_text(0, '543201')
+	l2.set_text(1, '345623')
+	l2.set_text(2, '9834')
+
+func refresh_entries():
+	Steam.downloadLeaderboardEntries(1, 10, Steam.LEADERBOARD_DATA_REQUEST_GLOBAL, SteamInit.leaderboard_handle)
+
+func on_leaderboard_score_uploaded(success, handle, score):
+	if success:
+		refresh_entries()
+	else:
+		printerr("failed uploading score")
+
+func submit_leaderboard_score():
+	var score = randi_range(0, 99999)
+	var name = Steam.getPersonaName()
+	Steam.uploadLeaderboardScore(score, true, PackedInt32Array(), SteamInit.leaderboard_handle)
+
+func update_leaderboard_scores(message: String, this_leaderboard_handle: int, results: Array) -> void:
+	for child in leaderboard_entries.get_children():
+		child.queue_free()
+	for result in results:
+		var row := data_row(result)
+		leaderboard_entries.add_child(row)
+		
+func data_row(entry) -> HBoxContainer:
+	var row := HBoxContainer.new()
+	row.add_child(data_cell(get_user_nickname(entry.steam_id)))
+	row.add_child(data_cell(entry.score))
+	row.add_child(data_cell(entry.global_rank))
+	return row
+
+func get_user_nickname(steam_id: int) -> String:
+	var name = Steam.getFriendPersonaName(steam_id)
+	return name if name else str(steam_id)
+
+func data_cell(data) -> Label:
+	var cell := Label.new()
+	cell.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	cell.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	cell.text = str(data)
+	return cell
