cc105ca9088d2c3d8b9c6341e7e4117590d5c646
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2025-01-20 05:28:10 +0800

    Added keybindings 1, 2, 3 to select level in level select

--- Components/UI/level_list_item.gd
+++ Components/UI/level_list_item.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends IMenuButton
 class_name LevelListItem
 
 @export var level_data: LevelData
@@ -11,5 +11,12 @@ func _ready():
 	mouse_entered.connect(func(): hover_frame.show())
 	focus_exited.connect(func(): hover_frame.hide())
 	mouse_exited.connect(func(): hover_frame.hide())
-	pressed.connect(func():
-		get_tree().change_scene_to_file(level_data.level_path))
+	pressed.connect(_load_scene())
+
+func _pressed():
+	_load_scene()
+	
+func _load_scene():
+	return func():
+		get_tree().change_scene_to_file(level_data.level_path)	
+	
